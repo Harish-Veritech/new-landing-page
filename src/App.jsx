87037cf2f83ba660d3cd7ff6625c7a1a602dfcf7
Harish Bisht <hbisht1@asu.edu>
2025-09-10 19:19:14 +0530

    added vite

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Helmet } from "react-helmet";
 // Screens
-import Landing from "./screens/Landing.jsx";
+import Landing from "./screens/Landing";
 
 export default function App() {
   return (
@@ -9,10 +9,12 @@ export default function App() {
       <Helmet>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-        <link href="https://fonts.googleapis.com/css2?family=Khula:wght@400;600;800&display=swap" rel="stylesheet" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Khula:wght@400;600;800&display=swap"
+          rel="stylesheet"
+        />
       </Helmet>
       <Landing />
     </>
   );
 }
-
